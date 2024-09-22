357f0df66ea34e7b1682a1681790f1a05824553c
Dave Roberts <d.roberts@ctrlo.com>
2024-09-22 18:23:59 +0100

    Initial commit

---
+++ src/main/kotlin/blog/davetheitguy/miniostorageprovider/MinioStorageProviderApplication.kt
@@ -0,0 +1,11 @@
+package blog.davetheitguy.miniostorageprovider
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinioStorageProviderApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinioStorageProviderApplication>(*args)
+}
